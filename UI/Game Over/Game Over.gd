a1acb9c1d153d0a975bd613c3f92ca90955e8b8e
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-07-15 12:56:44 +0100

    New graph

--- UI/Game Over/Game Over.gd
+++
@@ -1,6 +0,0 @@
-extends MarginContainer
-
-func _unhandled_input(_event):
-	if Input.is_action_just_pressed("restart"):
-		if get_tree().change_scene("res://Level/Level.tscn") != OK:
-			print_debug("An error occurred while attempting to change to the Level scene.")
--- Util/Util.gd
+++ Util/Util.gd
@@ -1,7 +1,9 @@
 extends Node
 
-func _ready():
-	randomize()
-
-func rand_element(array):
-	return array[randi() % array.size()]
+func range_float(start: float, end: float, step: float): # Credit: https://github.com/godotengine/godot/issues/4164#issuecomment-488754325
+	var res = Array()
+	var i = start
+	while i < end:
+		res.push_back(i)
+		i += step
+	return res
